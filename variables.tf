fd09b8da674d9a37c7a2abdfc460e0549808d1a1
SyrkashevAV <159136859+SyrkashevAV@users.noreply.github.com>
2024-03-16 15:23:19 +0300

    Update variables.tf
    Change3

--- variables.tf
+++ variables.tf
@@ -1,4 +1,52 @@
+variable "token" {
+  description = "Yandex Cloud security OAuth token"
+  default     = "y0_AgAAAAACcyKHAATuwQAAAAD49m_piftZgLAFTmuBYzDIbMlhXfAqMxU"
+}
 
+variable "folder_id" {
+  description = "Yandex Cloud Folder ID where resources will be created"
+  default     = "b1gf1njbp87lf6un2juk"
+}
+
+variable "zone" {
+  description = "Zone"
+  default = "ru-central1-a"
+}
+
+variable "image" {
+  description = "Specifying type of boot image"
+  default = "ubuntu-2204-lts"
+}
+
+variable "disk_size" {
+  description = "Specifying disk size"
+  default = 10
+}
+
+variable "private_key_path" {
+  description = "Path to ssh private key, which would be used to access workers"
+  default     = "~/.ssh/id_rsa"
+}
+
+variable "public_key_path" {
+  description = "Path to ssh public key"
+  default     = "~/.ssh/id_rsa.pub"
+}
+
+variable "maven_instance_name" {
+  description = "Name prefix for the nodes"
+  default = "maven-instance"
+}
+
+variable "tomcat_instance_name" {
+  description = "Name prefix for the nodes"
+  default = "tomcat-instance"
+}
+
+variable "num_nodes" {
+  description = "Number of nodes to create"
+  default = 1
+}
 
 
 
